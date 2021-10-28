940cf06735816c855078ea07dc866f074094cd2d
thadpearsall <thadpearsall@hotmail.com>
2021-10-28 11:31:12 -0400

    Initial Push and completed assignment all in one

---
+++ AssignmentClass.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RTSLabs_Assignment
+{
+    public class AssignmentClass
+    {
+
+        /// <summary>
+        /// evauluates the list
+        /// </summary>
+        /// <param name="intList">List of integers to compare</param>
+        /// <param name="comparisonValue">Value to compare against the list</param>
+        /// <returns>A list that contains two items, an above item and a below item</returns>
+        public static List<Tuple<string, int>> aboveBelow(List<int> intList, int comparisonValue)
+        {
+            var toRet = new List<Tuple<string, int>>();
+            if (intList == null || intList.Count == 0) return null;
+            var above = new Tuple<string, int>("above", intList.Where(i => i > comparisonValue).Count());
+            var below = new Tuple<string, int>("below", intList.Where(i => i < comparisonValue).Count());
+            toRet.Add(above);
+            toRet.Add(below);
+            return toRet;
+        }
+
+        public static string stringRotation(string strToRotate, int rotationCount)
+        {
+            string toRet = "";
+            if (strToRotate == null || strToRotate == "") return "";
+            if (strToRotate.Length <= rotationCount) return strToRotate; // can't rotate as the count is higher than the length of our string
+            if (rotationCount < 1) return strToRotate; //cant rotate as count is lower than possible so just return original
+            toRet = strToRotate.Substring(strToRotate.Length - rotationCount, rotationCount) + strToRotate.Substring(0,strToRotate.Length-rotationCount); //reorder the string
+            return toRet;
+        }
+
+    }
+}
---
+++ MainWindow.xaml.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace RTSLabs_Assignment
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public static List<int> testList;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            testList = new List<int>();
+            for (int x = 0; x < 20; x++)
+            {
+                testList.Add(x);
+            }
+        }
+
+        //wanted to finish and return the assignment quick, it didn't specify any UI requirements so i just tested code here
+        private void btn_AboveBelow_clicked(object sender, RoutedEventArgs e)
+        {
+            e.Handled = true;
+            var ret = AssignmentClass.aboveBelow(testList, 5);
+            
+            if (ret == null) return;
+
+            var first = ret.FirstOrDefault();
+            var second = ret.LastOrDefault();
+            if (first == null || second == null) return;
+            Debug.Print(first.Item1 + " " + first.Item2 + "\n" + second.Item1 + " " + second.Item2);
+            //Console.WriteLine(first.Item1 + " "+ first.Item2 + "\n" + second.Item1 + second.Item2);
+        }
+
+        private void btn_StringRotation_clicked(object sender, RoutedEventArgs e)
+        {
+            e.Handled = true;
+            var ret = AssignmentClass.stringRotation("StringToRotate", 5);
+            Debug.Print(ret);
+        }
+    }
+}
